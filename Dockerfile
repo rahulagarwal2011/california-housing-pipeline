fa4f20558809b80e3cd4b30cdaee405a78d859b5
rahulagarwal2011 <78501012+rahulagarwal2011@users.noreply.github.com>
2025-07-26 16:30:37 +0530

    fixed folder structure

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
-COPY . .
+COPY ./src /app/src
+COPY ./model /app/model
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
 
-CMD ["python", "predict.py"]
+CMD ["python", "src/predict.py"]
--- src/predict.py
+++ src/predict.py
@@ -5,12 +5,12 @@ from sklearn.model_selection import train_test_split
 
 model = joblib.load('model/california_housing_linear_regression_model.joblib')
 
+
 data = fetch_california_housing()
 X = data.data
 y = data.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 predictions = model.predict(X_test)
-
 
 print("Predictions on test set:", predictions)
